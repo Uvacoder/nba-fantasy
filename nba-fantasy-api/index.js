1ceb3b218a887fab3a65eb60b276d7fb016e0afa
Chy Meng <chymeng24@gmail.com>
2022-10-24 16:32:46 +1100

    feat: test

--- nba-fantasy-api/index.js
+++ nba-fantasy-api/index.js
@@ -20,10 +20,6 @@ app.use("/api", routes);
 // import environmental variables from our variables.env file
 require("dotenv").config({ path: "variables.env" });
 
-// READY?! Let's go!
-
-// Start our app!
-const app = require("./index");
 app.set("port", process.env.PORT || 7777);
 const server = app.listen(app.get("port"), () => {
   console.log(`Express running → PORT ${server.address().port}`);
